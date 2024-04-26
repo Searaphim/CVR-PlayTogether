ed106bdccee0d9327ce9d1878df2afd1809c6500
Searaphim <seara_send@hotmail.com>
2024-04-26 13:50:32 -0400

    Progress on sunshine implementation

--- PlayTogetherMain/PlayTogetherMod.cs
+++ PlayTogetherMain/PlayTogetherMod.cs
@@ -14,70 +14,35 @@ using System.IO.Compression;
 using System.IO;
 using System.Diagnostics;
 using MelonLoader.TinyJSON;
-using System.Runtime.InteropServices;
-
-/*[StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
-
-public class OpenFileName
-{
-    public int structSize = 0;
-    public IntPtr dlgOwner = IntPtr.Zero;
-    public IntPtr instance = IntPtr.Zero;
-    public String filter = null;
-    public String customFilter = null;
-    public int maxCustFilter = 0;
-    public int filterIndex = 0;
-    public String file = null;
-    public int maxFile = 0;
-    public String fileTitle = null;
-    public int maxFileTitle = 0;
-    public String initialDir = null;
-    public String title = null;
-    public int flags = 0;
-    public short fileOffset = 0;
-    public short fileExtension = 0;
-    public String defExt = null;
-    public IntPtr custData = IntPtr.Zero;
-    public IntPtr hook = IntPtr.Zero;
-    public String templateName = null;
-    public IntPtr reservedPtr = IntPtr.Zero;
-    public int reservedInt = 0;
-    public int flagsEx = 0;
-}
-
-public class DllTest
-{
-    [DllImport("Comdlg32.dll", SetLastError = true, ThrowOnUnmappableChar = true, CharSet = CharSet.Auto)]
-    public static extern bool GetOpenFileName([In, Out] OpenFileName ofn);
-    public static bool GetOpenFileName1([In, Out] OpenFileName ofn)
-    {
-        return GetOpenFileName(ofn);
-    }
-}*/
-
+using PlayTogetherMod.Utils;
+using System.Linq;
+using BTKUILib.UIObjects.Components;
 
 namespace PlayTogetherMod
 {
     public class SharedVars
     {
-        public const string RESOURCE_FOLDER = "Mods/CVRPlayTogether_Data";
-        public const string MOONLIGHT_PATH = RESOURCE_FOLDER + "/Moonlight/Moonlight.exe";
+        public const string RESOURCE_FOLDER = @"Mods\CVRPlayTogether_Data";
+        public const string MOONLIGHT_PATH = RESOURCE_FOLDER + @"\Moonlight\Moonlight.exe";
     }
 
 
 
     public class Sunshine : Process
     {
-        public const string EXE_PATH = SharedVars.RESOURCE_FOLDER + "/Sunshine/sunshine.exe";
-        public const string SETTINGS_PATH = SharedVars.RESOURCE_FOLDER + "/Sunshine/config/sunshine.conf"; //Multicast may or may not be necessary for multiple clients, idk.
-        public const string APPSCONF_PATH = SharedVars.RESOURCE_FOLDER + "/Sunshine/config/apps.json"; //Multicast may or may not be necessary for multiple clients, idk.
+        public const string EXE_PATH = SharedVars.RESOURCE_FOLDER + @"\Sunshine\sunshine.exe";
+        public const string SETTINGS_PATH = SharedVars.RESOURCE_FOLDER + @"\Sunshine\config\sunshine.conf"; //Multicast may or may not be necessary for multiple clients, idk.
+        public const string APPSCONF_PATH = SharedVars.RESOURCE_FOLDER + @"\Sunshine\assets\apps.json"; //Multicast may or may not be necessary for multiple clients, idk.
+        bool firstLaunch = true;
         //Apps file is only read on launch so sunshine needs to restart on edit. Probably the same for other confs.
         //channels=3 (3 distinct streams) //3 for 4 players
         //Configuration variables can be overwritten on the command line: "name=value" --> it can be usefull to set min_log_level=debug without modifying the configuration file
         public Sunshine() : base()
         {
             StartInfo.FileName = EXE_PATH;
+            StartInfo.WorkingDirectory = SharedVars.RESOURCE_FOLDER + @"\Sunshine";
             StartInfo.Arguments = "-p -0 -1"; //PINs can be written to stdin
+            StartInfo.RedirectStandardInput = true;
             //stdout will read "Please insert pin: " when ready to receive a pin through stdin
             //Make a warning for users to make sure apps they add are always launched fullscreen for privacy reasons. Atl-tabbing risky, theres probably a way to prevent desktop from streaming. (apps.json?)
         }
@@ -94,10 +59,10 @@ namespace PlayTogetherMod
             public string image_path;
         }
 
-        public class AppsConf
+        public struct AppsConf
         {
-            public List<string>? env;
-            public List<AppInfo>? apps;
+            public List<string> env;
+            public List<AppInfo> apps;
         }
 
         public AppsConf ReadAppsConf()
@@ -105,50 +70,55 @@ namespace PlayTogetherMod
             return JSON.Load(File.ReadAllText(APPSCONF_PATH)).Make<AppsConf>();
         }
 
+        public AppsConf BuildAppInfo(string appPath)
+        {
+            var conf = ReadAppsConf();
+            conf.env.Clear();
+            conf.apps.Clear();
+            conf.apps.Add(new AppInfo { name = Path.GetFileNameWithoutExtension(appPath), cmd = appPath });
+            return conf;
+        }
+
         public void WriteAppsConf(AppsConf appsConf)
         {
             File.WriteAllText(APPSCONF_PATH, JSON.Dump(appsConf, EncodeOptions.PrettyPrint));
         }
 
-        public void WritePin(string pin)
+        public void Run(string appPath)
         {
-            Console.WriteLine(pin);
-        }
-
-        public void Run(bool FirstRun = false)
-        {
-            if (FirstRun)
+            if (firstLaunch)
             {
-                WriteAppsConf(new AppsConf());
+                firstLaunch = false;
                 Start(StartInfo.FileName, "--creds defaultusr defaultpwd"); //Not a concern since we dont expose the interface to the internet
                 WaitForExit();
             }
+            WriteAppsConf(BuildAppInfo(appPath));
             Start();
             //Console.ReadLine(); 
         }
 
-        public void Restart()
+        public void Stop()
         {
             Close();
             WaitForExit();
-            Run();
         }
     }
 
     public class PlayTogether : MelonMod
     {
-        private Page _rootPage;
+        Page _rootPage;
         public const string PROP_SCENE = "AdditiveContentScene";
         //Root working dir at runtime is 'ChilloutVR'
         public const string MOONLIGHT_RESOURCE = "CVRPlayTogether.resources.MoonlightPortable-x64-5.0.1.zip";
         public const string SUNSHINE_RESOURCE = "CVRPlayTogether.resources.sunshine-windows-portable.zip";
-        //private Sunshine _sunshine = new Sunshine();
+        Sunshine _sunshine;
+        string pinInputs = "";
 
 
         private void UnpackResources()
         {
             ZipArchive zip = new ZipArchive(Assembly.GetExecutingAssembly().GetManifestResourceStream(MOONLIGHT_RESOURCE));
-            zip.ExtractToDirectory(SharedVars.RESOURCE_FOLDER + "/Moonlight", true);
+            zip.ExtractToDirectory(SharedVars.RESOURCE_FOLDER + @"\Moonlight", true);
             zip.Dispose();
             zip = new ZipArchive(Assembly.GetExecutingAssembly().GetManifestResourceStream(SUNSHINE_RESOURCE));
             zip.ExtractToDirectory(SharedVars.RESOURCE_FOLDER, true);
@@ -181,27 +151,62 @@ namespace PlayTogetherMod
                 }
             };
 
-            /*var hostCat = _rootPage.AddCategory("Game Hosting");
-            var buttonBrowse = hostCat.AddButton("Browse", "", "Select the game you wish to play");
-            buttonBrowse.OnPress += () =>
+            var hostCat = _rootPage.AddCategory("Game Hosting");
+            var pinPage = hostCat.AddPage("ENTER FRIEND PIN", "", "PIN", "CVRPlayTogether");
+            pinPage.Disabled = true;
+            var hostToggle = hostCat.AddToggle("Host", "Select the game and start hosting", false);
+            hostToggle.OnValueUpdated += b =>
             {
-                OpenFileName ofn = new OpenFileName();
-                ofn.structSize = Marshal.SizeOf(ofn);
-                ofn.filter = "All Files\0*.*\0\0";
-                ofn.file = new string(new char[256]);
-                ofn.maxFile = ofn.file.Length;
-                ofn.fileTitle = new string(new char[64]);
-                ofn.maxFileTitle = ofn.fileTitle.Length;
-                ofn.initialDir = UnityEngine.Application.dataPath;
-                ofn.title = "Upload Image";
-                ofn.defExt = "PNG";
-                ofn.flags = 0x00080000 | 0x00001000 | 0x00000800 | 0x00000200 | 0x00000008;//OFN_EXPLORER|OFN_FILEMUSTEXIST|OFN_PATHMUSTEXIST| OFN_ALLOWMULTISELECT|OFN_NOCHANGEDIR
-                if (DllTest.GetOpenFileName(ofn))
+                if (b == true)
                 {
-                    LoggerInstance.Msg($"Selected file: {ofn.file}");
-                    //FileDialogResult("file:///" + ofn.file);
+                    var filePath = FileBrowser.BrowseForFile();
+                    LoggerInstance.Msg($"Selected file: {filePath}");
+                    if (filePath != "")
+                    {
+                        _sunshine.Start();
+                        pinPage.Disabled = false;
+                    }
+                    else
+                    {
+                        hostToggle.ToggleValue = false;
+                    }
                 }
-            };*/
+                else
+                {
+                    _sunshine.Stop();
+                    pinPage.Disabled = true;
+                }
+            };
+            var pinKeyboard = pinPage.AddCategory("PIN KEYBOARD");
+            Category sendCat;
+            sendCat = pinPage.AddCategory("Enter PIN");
+            var sendButton = sendCat.AddButton("", "", "Validate a friend's PIN");
+            sendButton.OnPress += () =>
+            {
+                _sunshine.WritePin(pinInputs);
+                pinKeyboard.Disabled = false;
+                pinInputs = "";
+                sendButton.ButtonText = pinInputs;
+            };
+            List<Button> pinButtons = new List<Button>();
+            for(int i =0; i < 10; i++)
+            {
+                int buttonNumber = i;
+                pinButtons.Add(pinKeyboard.AddButton(buttonNumber.ToString(), "", ""));
+                pinButtons[buttonNumber].OnPress += () =>
+                {
+                    if (pinInputs.Length < 4)
+                    {
+                        pinInputs = pinInputs + buttonNumber.ToString();
+                        sendButton.ButtonText = pinInputs;
+                    }
+                    if (pinInputs.Length == 4)
+                    {
+                        sendButton.ButtonText = pinInputs;
+                        pinKeyboard.Disabled = true;
+                    }
+                };
+            }
         }
 
         public override void OnInitializeMelon()
@@ -213,7 +218,7 @@ namespace PlayTogetherMod
             //Assembly assembly = Assembly.GetAssembly(typeof(uWindowCapture.UwcWindowTexture));
             //propWhitelist.Add(assembly.GetType("uWindowCapture.UwcWindowTexture"));
 
-            //UI
+            _sunshine = new Sunshine();
             MakeUI();
         }
 
---
+++ PlayTogetherMain/Utils/Utils.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace PlayTogetherMod.Utils
+{
+    class FileBrowser
+    {
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+
+        public class OpenFileName
+        {
+            public int structSize = 0;
+            public IntPtr dlgOwner = IntPtr.Zero;
+            public IntPtr instance = IntPtr.Zero;
+            public string filter = null;
+            public string customFilter = null;
+            public int maxCustFilter = 0;
+            public int filterIndex = 0;
+            public string file = null;
+            public int maxFile = 0;
+            public string fileTitle = null;
+            public int maxFileTitle = 0;
+            public string initialDir = null;
+            public string title = null;
+            public int flags = 0;
+            public short fileOffset = 0;
+            public short fileExtension = 0;
+            public string defExt = null;
+            public IntPtr custData = IntPtr.Zero;
+            public IntPtr hook = IntPtr.Zero;
+            public string templateName = null;
+            public IntPtr reservedPtr = IntPtr.Zero;
+            public int reservedInt = 0;
+            public int flagsEx = 0;
+        }
+
+        public class DllTest
+        {
+            [DllImport("Comdlg32.dll", SetLastError = true, ThrowOnUnmappableChar = true, CharSet = CharSet.Auto)]
+            public static extern bool GetOpenFileName([In, Out] OpenFileName ofn);
+            public static bool GetOpenFileName1([In, Out] OpenFileName ofn)
+            {
+                return GetOpenFileName(ofn);
+            }
+        }
+
+        public static string BrowseForFile()
+        {
+            OpenFileName ofn = new OpenFileName();
+            ofn.structSize = Marshal.SizeOf(ofn);
+            ofn.filter = "Application (*.exe)\0*.exe\0\0";
+            ofn.file = new string(new char[256]);
+            ofn.maxFile = ofn.file.Length;
+            ofn.fileTitle = new string(new char[64]);
+            ofn.maxFileTitle = ofn.fileTitle.Length;
+            ofn.initialDir = UnityEngine.Application.dataPath;
+            ofn.title = "Select Game Executable";
+            ofn.defExt = "EXE";
+            ofn.flags = 0x00080000 | 0x00001000 | 0x00000800 | 0x00000008;//OFN_EXPLORER|OFN_FILEMUSTEXIST|OFN_PATHMUSTEXIST|OFN_NOCHANGEDIR
+            if (DllTest.GetOpenFileName(ofn))
+            {
+                return ofn.file;
+            }
+            return "";
+        }
+    }
+}
+
